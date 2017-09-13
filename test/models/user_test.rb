7c53565aa71fab98deb2487a4b432b0dc368bd14
Bryn Bayliss <brynbayliss@hotmail.com>
2017-09-13 19:37:20 +0100

    started tests and validation for users

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
+
+  def setup
+    @user = User.new(name: "Bryn", email: "email@example.com", password_digest: "password", address: "21 Fake Street", telephone: "0123456789")
+  end
+  test "should be valid?" do
+    assert @user.valid?
+  end
+
+  test "name should be present" do
+    @user.name = "  "
+    assert_not @user.valid?
+  end
+ end
