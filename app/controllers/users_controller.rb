98e8067617850222dc1d05d405d146f3e4ebcdac
Bryn Bayliss <brynbayliss@hotmail.com>
2017-09-14 21:19:54 +0100

    added form to views and flash messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,22 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Thanks for signing up"
+    else
+      render 'new'
+      flash[:fail] = "Sign up failed please try again"
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
+
+ def index
+ end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   valid_email_regex = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum: 255}, format: {with: valid_email_regex}, uniqueness: {case_sensitive: false}
   has_secure_password
-  validates :password_digest, presence: true, length: {minimum: 6, maximum: 50}
+  validates :password_digest, presence: true, length: {minimum: 6}
 end
