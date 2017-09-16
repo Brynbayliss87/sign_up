29d5c6445a5905bb8c90c0f2a5df1335f9d139c5
Bryn Bayliss <brynbayliss@hotmail.com>
2017-09-16 10:09:35 +0100

    updated gmail for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,14 +93,14 @@ Rails.application.configure do
   
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  
   config.action_mailer.smtp_settings {
-    :address  => 'smtp.gmail.com',
-    :port =>  587,
-    :enable_starttls_auto =>  true,
-    :username => ENV["gmail_username"],
-    :password =>  ENV["gmail_password"],
-    :authentication => :plain,
-    :domain =>  'gmail.com'
+    address:   'smtp.gmail.com',
+    port:   587,
+    enable_starttls_auto:  true,
+    user_name: ENV["gmail_username"],
+    password:  ENV["gmail_password"],
+    authentication:  :plain,
+    domain:  'gmail.com'
   }
 end
