032e5ad441ab1c7eaf3e91518a638ad151d53b06
Bryn Bayliss <brynbayliss@hotmail.com>
2017-09-17 19:59:47 +0100

    updated mailer config for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,8 +100,6 @@ Rails.application.configure do
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
-  :enable_starttls_auto => true,
+  :enable_starttls_auto => true
 }
-~    
-
 end
