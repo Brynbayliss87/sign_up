17c2a47b7697f1928b0e179c1d173c45449a9c05
Bryn Bayliss <brynbayliss@hotmail.com>
2017-09-17 21:09:11 +0100

    trying to get gmail to work

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,8 +96,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
   :user_name => ENV['SENDGRID_USERNAME'],
   :password => ENV['SENDGRID_PASSWORD'],
-  :domain => 'heroku.com',
-  :address => 'smtp.sendgrid.net',
+  :domain => 'gmail.com.com',
+  :address => 'smtp.gmail.com',
   :port => 587,
   :authentication => :plain,
   :enable_starttls_auto => true
