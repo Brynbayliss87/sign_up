38ad91e5684da80705943686e71cbb3c3ef938e1
Bryn Bayliss <brynbayliss@hotmail.com>
2017-09-17 20:01:15 +0100

    updated mailer config for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
 
-  confi.action_mailer.default_url = {:host => 'tranquil-castle-95937.herokuapp.com'}
+  config.action_mailer.default_url = {:host => 'tranquil-castle-95937.herokuapp.com'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
