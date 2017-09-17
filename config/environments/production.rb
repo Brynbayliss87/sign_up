5f0d6251c92898e6f30a9dd01243b6502a13695e
Bryn Bayliss <brynbayliss@hotmail.com>
2017-09-17 21:13:09 +0100

    trying to get gmail to work

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,8 +94,8 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
-  :user_name => ENV['SENDGRID_USERNAME'],
-  :password => ENV['SENDGRID_PASSWORD'],
+  :user_name => ENV['GMAIL_USERNAME'],
+  :password => ENV['GMAIL_PASSWORD'],
   :domain => 'gmail.com.com',
   :address => 'smtp.gmail.com',
   :port => 587,
